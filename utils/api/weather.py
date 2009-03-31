94dbdcb96ae6c6b56653edb28540d51295265c90
mrchucho <mrchucho@mrchucho.net>
2009-03-30 22:01:39 -0500

    Was not properly importing models.CAPError

--- utils/api/weather.py
+++ utils/api/weather.py
@@ -1,11 +1,13 @@
 from google.appengine.api import urlfetch
 
+from models import CAPError
+
 def cap_for_state(state):
   try:
     result = urlfetch.fetch("http://www.weather.gov/alerts/%s.cap" % state.lower())
     if result.status_code != 200:
-      raise models.CAPError(result.status_code)
+      raise CAPError(result.status_code)
     else:
       return result.content
   except urlfetch.DownloadError:
-    raise models.CAPError
+    raise CAPError
